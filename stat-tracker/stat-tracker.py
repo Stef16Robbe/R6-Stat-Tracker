ec5bc36d2f5aa12b2a5c224fd8fb75eabec901d7
Stef <stef.robbe@gmail.com>
2020-07-09 13:25:53 +0200

    thresholding

--- stat-tracker/stat-tracker.py
+++ stat-tracker/stat-tracker.py
@@ -2,47 +2,30 @@ import cv2
 import pytesseract
 
 
-# pytesseract.image_to_string(img, config='--psm 6')
-
 def read_img():
     pytesseract.pytesseract.tesseract_cmd = 'C:\\Program Files (x86)\\Tesseract-OCR\\tesseract.exe'
-    img = cv2.imread('Images/Image3-S.png')
-    return cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    img = cv2.imread('Images/Image4.png', cv2.IMREAD_GRAYSCALE)
+    # tozero
+    _, th1 = cv2.threshold(img, 127, 255, cv2.THRESH_BINARY)
+    return th1, img
 
 
 def process_text(img):
-    names = []
+    values = []
     data = pytesseract.image_to_data(img)
     for x, d in enumerate(data.splitlines()):
         if x != 0:
             d = d.split()
             if len(d) == 12:
-                names.append(d[11])
+                values.append(d[11])
 
-    return names
-
-
-def process_numbers(img):
-    numbers = []
-    conf = r'--oem 3 --psm 6 outbase digits'
-    data = pytesseract.image_to_data(img, config=conf)
-    for x, d in enumerate(data.splitlines()):
-        if x != 0:
-            d = d.split()
-            if len(d) == 12:
-                numbers.append(d[11])
-
-    return numbers
+    return values
 
 
-def show_img(img):
-    cv2.imshow('Result', img)
-    cv2.waitKey(0)
+th1, img = read_img()
+print(process_text(th1))
+cv2.imshow('th1', th1)
+cv2.imshow('img', img)
 
-
-img = read_img()
-print(process_text(img))
-# print(process_numbers(img))
-show_img(img)
-
-
+cv2.waitKey(0)
+cv2.destroyAllWindows()
