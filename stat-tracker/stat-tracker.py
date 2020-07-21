07767cd634d53d5047735606e98092d498600ab2
Stef <stef.robbe@gmail.com>
2020-07-21 15:57:20 +0200

    Improved insanely (ty stackoverflow :D)

--- stat-tracker/stat-tracker.py
+++ stat-tracker/stat-tracker.py
@@ -1,25 +1,14 @@
 import cv2
 import pytesseract
-import numpy as np
 
 
-pytesseract.pytesseract.tesseract_cmd = 'C:\\Program Files (x86)\\Tesseract-OCR\\tesseract.exe'
-img = cv2.imread('Images/Image4.png', cv2.IMREAD_GRAYSCALE)
-# tozero
-_, th1 = cv2.threshold(img, 127, 255, cv2.THRESH_BINARY_INV)
+def read_score():
+    img = cv2.imread('Images/Image5.png', cv2.IMREAD_GRAYSCALE)
 
-values = []
-data = pytesseract.image_to_data(img)
-for x, d in enumerate(data.splitlines()):
-    if x != 0:
-        d = d.split()
-        if len(d) == 12:
-            values.append(d[11])
+    thresh = cv2.threshold(img, 100, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
+    thresh = cv2.resize(thresh, (0, 0), fx=1.25, fy=1.25)  # scale image 1.25X
+
+    return pytesseract.image_to_string(thresh, config='--psm 6')
 
 
-print(values)
-cv2.imshow('th1', th1)
-cv2.imshow('img', img)
-
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+scores = read_score()
