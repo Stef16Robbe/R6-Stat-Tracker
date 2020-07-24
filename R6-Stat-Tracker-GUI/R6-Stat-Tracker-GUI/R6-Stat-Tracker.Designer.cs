e60eee585e6bb91d12e6da0518a1ddb598a4b14e
Stef Robbe <627468@student.inholland.nl>
2020-07-24 11:43:48 +0200

    Fix + some testing

--- R6-Stat-Tracker-GUI/R6-Stat-Tracker-GUI/R6-Stat-Tracker.Designer.cs
+++ R6-Stat-Tracker-GUI/R6-Stat-Tracker-GUI/R6-Stat-Tracker.Designer.cs
@@ -58,7 +58,7 @@
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Location = new System.Drawing.Point(162, 32);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(545, 304);
+            this.dataGridView1.Size = new System.Drawing.Size(545, 258);
             this.dataGridView1.TabIndex = 3;
             // 
             // Form1
